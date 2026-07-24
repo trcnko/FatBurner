e492e8d3ca58d16b5c154884c14935e39d7bf9ad
Aleksandra Trotsenko <trcnko@MacBook-Air-Aleksandra.local>
2026-07-24 20:17:28 +0300

    optimize main.py and add delete meal endpoint

--- main.py
+++ main.py
@@ -7,6 +7,37 @@ from datetime import date
 
 app = FastAPI()
 
+def calculate_nutrition(meals, daily_target):
+    cpfc = {}
+
+    cpfc['ККалории'] = 0
+    cpfc['Белки'] = 0
+    cpfc['Жиры'] = 0
+    cpfc['Углеводы'] = 0
+
+    history_list = []
+    for meal in meals:
+        cpfc['ККалории'] += meal.product.calories * meal.grams * 0.01
+        cpfc['Белки'] += meal.product.proteins * meal.grams * 0.01
+        cpfc['Жиры'] += meal.product.fats * meal.grams * 0.01
+        cpfc['Углеводы'] += meal.product.carbs * meal.grams * 0.01
+
+        meal_calories = round(meal.product.calories * meal.grams * 0.01)
+        meal_proteins = round(meal.product.proteins * meal.grams * 0.01, 1)
+        meal_fats = round(meal.product.fats * meal.grams * 0.01, 1)
+        meal_carbs = round(meal.product.carbs * meal.grams * 0.01, 1)
+
+        history_list.append({
+            'Продукт': meal.product.name,
+            'Калории, ккал': meal_calories,
+            'Белки, г': meal_proteins,
+            'Жиры, г': meal_fats,
+            'Углеводы, г': meal_carbs
+        })
+
+    remains = daily_target - cpfc['ККалории']
+
+    return cpfc, history_list, remains
 
 def get_db():
     db = SessionLocal()
@@ -52,17 +83,14 @@ def create_user(user_in: UserCreate, db: Session = Depends(get_db)):
 def create_meal(meal_in: MealCreate, db: Session = Depends(get_db)):
     user = db.query(Users).filter(Users.name == meal_in.user_name).first()
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
 
     product = db.query(Products).filter(Products.product_id == meal_in.product_id).first()
     if not product:
-        raise HTTPException(status_code=404, detail="Product not found")
+        raise HTTPException(status_code=404, detail="Продукт не найден")
 
     db_meal = Meals(user_id=user.user_id,
-                    product_id=meal_in.product_id,
-                    grams=meal_in.grams,
-                    date=meal_in.date)
-
+                    **meal_in.model_dump())
     db.add(db_meal)
     db.commit()
     db.refresh(db_meal)
@@ -71,7 +99,6 @@ def create_meal(meal_in: MealCreate, db: Session = Depends(get_db)):
 
 @app.get('/users/{name}/stats')
 def get_user_stats(name: str, date_query: date, db: Session = Depends(get_db)):
-
     user = db.query(Users).filter(Users.name == name).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -80,39 +107,26 @@ def get_user_stats(name: str, date_query: date, db: Session = Depends(get_db)):
                                           Meals.date == date_query
                                           ).all()
 
-    cpfc = {}
+    total_eaten, meals_history, remains = calculate_nutrition(meals, user.daily_target)
 
-    cpfc['ККалории'] = 0
-    cpfc['Белки'] = 0
-    cpfc['Жиры'] = 0
-    cpfc['Углеводы'] = 0
-
-    history_list = []
-    for meal in meals:
-        cpfc['ККалории'] += meal.product.calories * meal.grams * 0.01
-        cpfc['Белки'] += meal.product.proteins * meal.grams * 0.01
-        cpfc['Жиры'] += meal.product.fats * meal.grams * 0.01
-        cpfc['Углеводы'] += meal.product.carbs * meal.grams * 0.01
-
-        meal_calories = round(meal.product.calories * meal.grams * 0.01) # Целые калории
-        meal_proteins = round(meal.product.proteins * meal.grams * 0.01, 1)
-        meal_fats = round(meal.product.fats * meal.grams * 0.01, 1)
-        meal_carbs = round(meal.product.carbs * meal.grams * 0.01, 1)
-
-        history_list.append({
-            'Продукт': meal.product.name,
-            'Калории, ккал': meal_calories,
-            'Белки, г': meal_proteins,
-            'Жиры, г': meal_fats,
-            'Углеводы, г': meal_carbs
-        })
-
-    remains = user.daily_target - cpfc['ККалории']
     return {
         'user_name': user.name,
         'date': date_query,
         'daily_target': user.daily_target,
-        'total_eaten': cpfc,
+        'total_eaten': total_eaten,
         'remains': remains,
-        'meals_history': history_list
+        'meals_history': meals_history
     }
+
+
+@app.delete('/meals/{meal_id}')
+def delete_meal(meal_id: int, db: Session = Depends(get_db)):
+    meal = db.query(Meals).filter(Meals.meal_id == meal_id).first()
+
+    if not meal:
+        raise HTTPException(status_code=404, detail='Приём пищи не найден')
+
+    db.delete(meal)
+    db.commit()
+
+    return {'message': 'Приём пищи успешно удалён'}
